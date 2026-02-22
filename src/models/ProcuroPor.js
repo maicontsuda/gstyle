66b1e7f2abefe4ab5bd3f9c5bebe4fdf73be0d82
maicontsuda <maicontsuda@gmail.com>
2026-02-22 22:09:07 +0900

    Create ProcuroPor.js model for customer search requests

---
+++ src/models/ProcuroPor.js
@@ -0,0 +1,12 @@
+class ProcuroPor {
+  constructor(marca, modelo, ano, km, cor, preco) {
+    this.marca = marca;
+    this.modelo = modelo;
+    this.ano = ano;
+    this.km = km;
+    this.cor = cor;
+    this.preco = preco;
+  }
+}
+
+module.exports = ProcuroPor;
